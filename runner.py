0894df95f94d63e19d00462d8626d3c244825ebf
Ray Chen <tankimarshal2@gmail.com>
2026-08-28 23:31:33 +0100

    Add arguments to customize runner

--- runner.py
+++ runner.py
@@ -2,19 +2,19 @@ from mechsim import Expression, Solver
 from pyscript import window, ffi, web
 
 main_solver = None
-energy_label = None
+energy_label = web.page["#energy-label"]
 
-STEPS = 20
+steps = 20
 dt = 1/60
 last = 0
 def update(timestamp):
     global last
-    progress = min(timestamp / 1000 - last, dt)
+    progress = min(timestamp / 1000 - last, dt + 1e-6)
     last = timestamp / 1000
 
-    while progress > dt / STEPS:
-        progress -= dt / STEPS
-        main_solver.step(dt / STEPS)
+    while progress > dt / steps:
+        progress -= dt / steps
+        main_solver.step(dt / steps)
 
     window.drawSystem(ffi.to_js(main_solver.get_params()))
     window.requestAnimationFrame(ffi.create_proxy(update))
@@ -22,18 +22,20 @@ def update(timestamp):
     t, v = main_solver.get_energies()
     energy_label.innerHTML = f"Kinetic: {t:.2f} | Potential: {v:.2f} | Total: {t + v:.2f}"
 
-def load_solver(solver):
-    global main_solver, energy_label
-
+def load_solver(solver, custom_steps=None, render_equations=True):
+    global main_solver, steps
     main_solver = solver
-    Expression.latex_mode = True
-    solver2 = Solver(*solver.original)
-    solver2.load_constants({})
+    if custom_steps is not None:
+        steps = custom_steps
 
-    latex = "\\begin{align*}"
-    latex += "\\\\".join(solver2.display_equations())
-    latex += "\\end{align*}"
-    window.setEquationlabel(latex)
+    if render_equations:
+        Expression.latex_mode = True
+        solver2 = Solver(*solver.original)
+        solver2.load_constants({})
 
-    energy_label = web.page["#energy-label"]
+        latex = "\\begin{align*}"
+        latex += "\\\\".join(solver2.display_equations())
+        latex += "\\end{align*}"
+        window.setEquationlabel(latex)
+
     window.requestAnimationFrame(ffi.create_proxy(update))
