c2e212944933f43706301ee2f1c5c080278c946c
Ray Chen <tankimarshal2@gmail.com>
2026-09-09 21:40:21 +0100

    Implement simple pendulum

---
+++ dpend/system.py
@@ -0,0 +1,17 @@
+from mechsim import Expression, Var, Solver
+from mechsim.system import Mass, Vector, System
+
+Expression.context = ["theta1", "theta2"]
+mass1 = Mass("m1")
+mass1.constrain_hinge("theta1", Vector(0, -Var("l1")))
+mass2 = Mass("m2")
+mass2.constrain_hinge("theta2", Vector(0, -Var("l2")), mass1.position)
+
+system = System(mass1, mass2)
+solver = Solver(system.kinetic(), system.potential())
+solver.load_constants({"m1": 1, "m2": 1, "g": 10, "l1": 0.5, "l2": 0.5})
+solver.load_initial_values([2, 3.14])
+
+if __name__ == "__main__":
+    from runner import load_solver
+    load_solver(solver)
