bc2793ae10040459956b614bde09d51ef91478df
Ray Chen <tankimarshal2@gmail.com>
2026-09-16 17:15:02 +0100

    Add cart pendulum sim

---
+++ cart/system.py
@@ -0,0 +1,18 @@
+from mechsim import Expression, Var, Solver
+from mechsim.system import Mass, Spring, Vector, System
+
+Expression.context = ["x", "theta"]
+cart = Mass("M")
+cart.constrain_plane("x", Vector(1, 0), Vector(0, 0.5))
+pendulum = Mass("m")
+pendulum.constrain_hinge("theta", Vector(0, -Var("l")), cart.position)
+spring = Spring(Vector(-Var("d"), 0.5), cart.position, Var("d"), Var("k"))
+
+system = System(cart, pendulum, spring)
+solver = Solver(system.kinetic(), system.potential())
+solver.load_constants({"M": 1, "m": 1, "g": 10, "l": 1, "d": 1.5, "k": 6})
+solver.load_initial_values([-1, 0.5])
+
+if __name__ == "__main__":
+    from runner import load_solver
+    load_solver(solver)
