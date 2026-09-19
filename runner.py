7bff1d01d247513c93e236e46c6cc5270bf51428
Ray Chen <tankimarshal2@gmail.com>
2026-09-19 17:46:41 +0100

    Add playback control api

--- runner.py
+++ runner.py
@@ -7,20 +7,37 @@ energy_label = web.page["#energy-label"]
 steps = 20
 dt = 1/60
 last = 0
+paused = False
 def update(timestamp):
     global last
     progress = min(timestamp / 1000 - last, dt + 1e-6)
     last = timestamp / 1000
+
+    if paused:
+        return
 
     while progress > dt / steps:
         progress -= dt / steps
         main_solver.step(dt / steps)
 
     window.drawSystem(ffi.to_js(main_solver.get_params()))
-    window.requestAnimationFrame(ffi.create_proxy(update))
+    request_update()
 
     t, v = main_solver.get_energies()
     window.setEnergyLabel(t, v)
+
+def request_update():
+    window.requestAnimationFrame(ffi.to_js(update))
+
+def toggle_playback():
+    global paused
+    paused = not paused
+    if not paused:
+        request_update()
+
+def step_playback():
+    if paused:
+        request_update()
 
 def load_solver(solver, custom_steps=None, render_equations=True):
     global main_solver, steps
@@ -38,4 +55,4 @@ def load_solver(solver, custom_steps=None, render_equations=True):
         latex += "\\end{align*}"
         window.setEquationlabel(latex)
 
-    window.requestAnimationFrame(ffi.create_proxy(update))
+    request_update()
